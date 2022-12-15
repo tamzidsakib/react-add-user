cd9efd3afbf9421b44498c832f9b35aaa580a0c6
tamzid sakib <tmsakib@gmail.com>
2022-12-15 15:57:39 +0600

    card

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "../UI/Card";
+
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <Card>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add user</button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
